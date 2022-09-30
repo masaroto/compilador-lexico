6388e6bb2d89f9711ff595de7dda9c869d924dde
masaroto <pedrolandins@gmail.com>
2022-09-29 21:36:39 -0300

    teste

--- lex.py
+++ lex.py
@@ -1,37 +1,32 @@
 import re
 
-# num_real = re.compile(rf'{num.pattern},[\d]+')
-# lista_ident = re.compile(rf'{ident.pattern}[,{ident.pattern}]*')
-# fator = re.compile(rf'{ident.pattern}|{num.pattern}')
-txt = "x = 12 * 3\n b = a * 2" 
+txt = "x = 12 * 3\nb = a * 2\n//teste aaifbisbfgioafbgiabsdfpibsdfisbdnfipbpibpibpii sabvdoB !@#% ¨& * ()-+=§\n" 
 
+coment = re.compile(r'//.*[\n]$') #ER pra achar comentario
 num = re.compile(r'^[\d]+[,\d+]?')
 ident = re.compile(r'^[a-zA-Z][a-zA-Z0-9]*')
 op_arit = re.compile(r'^[\*|\/|\+|\-]')
 op_rel = re.compile(r'^[\=|\<\>|\<|\<\=|\>\=|\>]')
 tipo = re.compile(r'^[INTEGER|REAL|STRING]')
-coment = re.compile(r'^[\/\/].') #fazer depois pra ignorar comentario
 abre_paren = re.compile(r'^\(')
 fecha_parent = re.compile(r'^\)')
 
 nome_regra = ["tipo", "num", "ident", "op_arit", "op_rel"]
 regras = [tipo, num, ident, op_arit, op_rel]
 
-print(txt)
+txt = re.sub(coment, "", txt)
+# print(txt)
 
 while txt!="":    
     txt = re.sub(r"^\s+", "", txt, 1)
-    # print(txt)
     for x in range (0, len(regras)):
         regra = regras[x]
-        # print(regra.pattern)
         lexema = regra.search(txt)
         if lexema: 
-            print("token:",lexema.group())
-            print("regra:", nome_regra[x])
+            print("token:",lexema.group(), "| regra:", nome_regra[x])
             txt = txt[lexema.end():]
             break
     if (not lexema):
         if txt != "":
-            print(txt[0], "<- esse simbolo não pertence a linguagem")
+            print("Erro: ", txt[0], "<- esse simbolo não pertence a linguagem")
             break
