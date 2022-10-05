55b7d3e6dc4e227e090877ca66370dbdf0f049b5
Gwanke <ra91671@uem.br>
2022-10-05 15:53:03 -0300

    Pasta exemplos, leitura teste 1 certa

--- lex.py
+++ lex.py
@@ -1,22 +1,50 @@
 import re
+import os
 
-txt = "x = 12 * 3\nb = a * 2\n//teste aaifbisbfgioafbgiabsdfpibsdfisbdnfipbpibpibpii sabvdoB !@#% ¨& * ()-+=§\n" 
+path = "exemplos/maior.ilp"
 
-coment = re.compile(r'//.*[\n]$') #ER pra achar comentario
+if os.path.isfile(path):
+    file = open(path, "r")
+    txt = str(file.read())
+    file.close()
+
+
+
+coment = re.compile(r'//.*[\n]') #ER pra achar comentario
+pro = re.compile(r'^PROGRAM')
+wri = re.compile(r'^WRITE')
+end = re.compile(r'^END')
+beg = re.compile(r'^BEGIN')
+pv = re.compile(r'^;')
+doisp = re.compile(r'^:')
+vir = re.compile(r'^,')
+var = re.compile(r'^VAR')
+se = re.compile(r'^IF')
+ent = re.compile(r'^ELSE')
+the = re.compile(r'^THEN')
 num = re.compile(r'^[\d]+[,\d+]?')
 ident = re.compile(r'^[a-zA-Z][a-zA-Z0-9]*')
-op_arit = re.compile(r'^[\*|\/|\+|\-]')
-op_rel = re.compile(r'^[\=|\<\>|\<|\<\=|\>\=|\>]')
-tipo = re.compile(r'^[INTEGER|REAL|STRING]')
+op_arit = re.compile(r'^\*|^\/|^\+|^\-')
+op_rel = re.compile(r'^\=|^\<\>|^\<|^\<\=|^\>\=|^\>')
+tipo = re.compile(r'^INTEGER|^REAL|^STRING')
 abre_paren = re.compile(r'^\(')
 fecha_parent = re.compile(r'^\)')
 
-nome_regra = ["tipo", "num", "ident", "op_arit", "op_rel"]
-regras = [tipo, num, ident, op_arit, op_rel]
+nome_regra = ["pro", "wri", "end", "beg",
+              "pv", "doisp", "vir",
+              "var", "num", "se", "ent", "the",
+              "ident", "op_arit", "op_rel",  
+              "abre_paren", "fecha_parent", "tipo"]
+
+              
+regras = [pro, wri, end, beg, 
+          pv,doisp, vir, 
+          var, tipo, se, ent, the,
+          ident, op_arit, op_rel, 
+          abre_paren, fecha_parent, tipo]
 
 txt = re.sub(coment, "", txt)
-# print(txt)
-
+print(txt)
 while txt!="":    
     txt = re.sub(r"^\s+", "", txt, 1)
     for x in range (0, len(regras)):
